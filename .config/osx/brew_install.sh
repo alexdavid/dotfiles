2088ecab03e748911bfd53c2283ce5f6dc148e07
Alex David <alex@Alexs-MacBook-Pro.local>
2013-07-19 15:31:20 -0700

    Add brew install script.

---
+++ .config/osx/brew_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+brew install \
+  automake \
+  findutils \
+  fish \
+  git-extras \
+  hub \
+  nodejs \
+  openssl \
+  png2ico \
+  postgresql \
+  rbenv \
+  rename \
+  ruby-build\
+  wget
