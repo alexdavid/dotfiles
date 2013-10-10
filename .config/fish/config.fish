de4acd481455cbaa4a60867483b48647c653181a
Alex David <github.com@alexd.me>
2013-10-09 23:45:47 -0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,8 @@
-set -x PATH ~/.bin ~/.rvm/bin /usr/local/bin /usr/local/share/npm/bin ~/.rvm/gems/ruby-1.9.3-p327/bin ./node_modules/.bin $PATH
+set -x PATH ~/.bin ~/.rbenv/shims /usr/local/bin /usr/local/share/npm/bin ./node_modules/.bin $PATH
 set fish_greeting ""
 
 function fish_title
   prompt_pwd
 end
 
-set -x PATH ~/.rbenv/shims $PATH
 rbenv rehash
