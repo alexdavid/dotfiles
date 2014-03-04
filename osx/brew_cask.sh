a7d6196558ae0491daf3073a0a2d59b3e8407bc9
Alex David <github.com@alexd.me>
2014-03-04 00:31:52 -0800

    Create brew_cask.sh

---
+++ osx/brew_cask.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+brew cask install \
+  dropbox \
+  google-chrome \
+  iterm2 \
+  macvim \
+  qlcolorcode \
+  qlstephen \
+  sequel-pro \
+  true-crypt \
+  vlc
