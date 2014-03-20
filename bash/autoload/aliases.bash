a097f2b4bc10e944ac671772203078214300153e
Alex David <alex.david@originate.com>
2014-03-20 00:41:04 -0700

    Clean up bash config

--- bash/autoload/aliases.bash
+++ bash/autoload/aliases.bash
@@ -1,9 +1,2 @@
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-alias fn='find . -name'
 alias ..='cd ..'
-alias gg='cd .. && ls'
-
-# I prefer grep colors to boring pgrep
-alias pgrep='ps -efM | grep'
+alias ...='cd ../..'
