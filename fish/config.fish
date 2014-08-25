d458d52771b5d8ea7835016afc774ba860365969
Alex David <alex.david@originate.com>
2014-08-24 20:14:47 -0700

    Fix fish shell if rbenv isn't installed

--- fish/config.fish
+++ fish/config.fish
@@ -12,8 +12,9 @@ function fish_title
   prompt_pwd
 end
 
-rbenv rehash
-refresh-colors
+if which rbenv > /dev/null
+  rbenv rehash
+end
 
 # Load custom settings for current user
 set local_settings_file ~/config.fish.local
@@ -21,3 +22,5 @@ set local_settings_file ~/config.fish.local
 if test -f $local_settings_file
    . $local_settings_file
 end
+
+refresh-colors
