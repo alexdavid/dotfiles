927c867c0db39de256ac39a9e207cbf02202a822
Alex David <git@alexd.me>
2017-07-21 17:17:15 -0700

    Clean up brew installs

--- osx/brew_install.sh
+++ osx/brew_install.sh
@@ -6,19 +6,13 @@ brew install \
   ack \
   automake \
   fish \
+  git \
   git-branch-status \
   git-town \
   htop \
-  libevent \
-  nodejs \
   openssl \
-  postgresql \
   python \
-  rbenv \
   reattach-to-user-namespace \
-  rename \
-  ruby-build \
-  shpotify \
   the_silver_searcher \
   vim \
   wget
