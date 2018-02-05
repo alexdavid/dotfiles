a221b2d21aff706ffa246847cb4c7b3957de047d
Alex David <alex.david@originate.com>
2018-02-05 13:03:29 -0800

    Add main firefox profile

---
+++ mozilla/main/user.js
@@ -0,0 +1,16 @@
+// General
+user_pref("browser.privatebrowsing.autostart", true);
+user_pref("browser.search.defaultenginename", "DuckDuckGo");
+user_pref("browser.search.widget.inNavBar", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("general.warnOnAboutConfig", false);
+
+// New tab page
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.migrationExpired", true);
+user_pref("browser.newtabpage.activity-stream.prerender", false);
+user_pref("browser.newtabpage.activity-stream.showSearch", true);
+user_pref("browser.newtabpage.activity-stream.showTopSites", false);
+user_pref("browser.uiCustomization.state", '{"placements":{"widget-overflow-fixed-list":[],"PersonalToolbar":["personal-bookmarks"],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","search-container","downloads-button"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"toolbar-menubar":["menubar-items"]},"seen":["developer-button"],"dirtyAreaCache":["PersonalToolbar","nav-bar","TabsToolbar","toolbar-menubar"],"currentVersion":14,"newElementCount":2}');
