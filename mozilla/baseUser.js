cca10410032e84004fb1d9f8da5ce89d9af5bfa1
Alex David <alex.david@originate.com>
2018-02-13 13:54:19 -0800

    Clean up firefox profiles

--- mozilla/baseUser.js
+++ mozilla/baseUser.js
@@ -1,7 +1,5 @@
 // General
-user_pref("browser.privatebrowsing.autostart", true);
 user_pref("browser.search.defaultenginename", "DuckDuckGo");
-user_pref("browser.search.widget.inNavBar", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("general.warnOnAboutConfig", false);
 user_pref("signon.rememberSignons", false);
@@ -14,4 +12,10 @@ user_pref("browser.newtabpage.activity-stream.migrationExpired", true);
 user_pref("browser.newtabpage.activity-stream.prerender", false);
 user_pref("browser.newtabpage.activity-stream.showSearch", true);
 user_pref("browser.newtabpage.activity-stream.showTopSites", false);
-user_pref("browser.uiCustomization.state", '{"placements":{"widget-overflow-fixed-list":[],"PersonalToolbar":["personal-bookmarks"],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","search-container","downloads-button"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"toolbar-menubar":["menubar-items"]},"seen":["developer-button"],"dirtyAreaCache":["PersonalToolbar","nav-bar","TabsToolbar","toolbar-menubar"],"currentVersion":14,"newElementCount":2}');
+
+// Prevent first run
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("startup.homepage_welcome_url", "about:blank");
+
+// Clean up nav bar
+user_pref("browser.uiCustomization.state", '{"placements":{"widget-overflow-fixed-list":[],"PersonalToolbar":[],"nav-bar":[],"TabsToolbar":[],"toolbar-menubar":[]}}');
