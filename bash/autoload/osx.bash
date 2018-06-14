468349b40648f4bb923781d872218009261d1c3d
Alex David <alex.david@originate.com>
2018-06-13 20:11:40 -0700

    Remove bash config

--- bash/autoload/osx.bash
+++
@@ -1,9 +0,0 @@
-if [ "$UNAME" = Darwin ]; then
-  # add color to ls
-  alias ls='ls -G'
-  # add color to grep
-  alias grep='grep --color=auto'
-  alias fgrep='fgrep --color=auto'
-  alias egrep='egrep --color=auto'
-fi
-
--- bash/init.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-# Create symlinks
-ln -s .config/bash/bashconfig ~/.bash_profile
-ln -s .config/bash/bashconfig ~/.bashrc
