877688a4e4db291deecbf969c0ac120c7f33eb61
Alex David <alex.david@originate.com>
2018-08-19 12:21:17 -0700

    Update vim init script

--- vim/init.sh
+++ vim/init.sh
@@ -2,8 +2,3 @@
 
 # Create symlinks
 ln -s .config/vim/vimrc ~/.vimrc
-
-# Install vim plugins
-mkdir ~/.config/vim/plug.vim
-curl -fLo ~/.config/vim/plug.vim/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
