1cbb2ddcf3489b5f0b1a62c8beca05c2cdec337b
Alex David <alex.david@originate.com>
2018-09-09 09:51:22 -0700

    Update qutebrowser config

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -4,11 +4,11 @@ c.content.javascript.enabled = False
 c.editor.command = ['popup-term', 'vim {file} -c "normal {line}G{column0}l"']
 c.editor.encoding = 'utf-8'
 
-config.set('content.javascript.enabled', True, '*://github.com/*')
-config.set('content.javascript.enabled', True, '*://localhost/*')
-config.set('content.javascript.enabled', True, '*://*.duckduckgo.com/*')
 config.set('content.javascript.enabled', True, 'chrome://*/*')
 config.set('content.javascript.enabled', True, 'file://*')
+config.set('content.javascript.enabled', True, 'http://localhost/*')
+config.set('content.javascript.enabled', True, 'https://*.duckduckgo.com/*')
+config.set('content.javascript.enabled', True, 'https://github.com/*')
 config.set('content.javascript.enabled', True, 'qute://*/*')
 
 # Make :q work more like vim:
