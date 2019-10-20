c2973393988a69238e589e9bc537b9543cd0d51a
Alex David <alex.david@originate.com>
2019-10-20 09:47:15 -0700

    Only display calendar block if khal is installed

--- i3blocks/calendar.sh
+++ i3blocks/calendar.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+which khal || exit
+
 event="$(khal list --day-format '' --format '{title} @ {start-time}' now eod)"
 if [ "$event" != "No events" ]; then
   echo " $event" | tail +1
