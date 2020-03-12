35b3dbe40b2f88b72140db393c59a9350a606635
Alex David <alex.david@originate.com>
2020-03-11 19:47:05 -0700

    Fix blocks mpd for env var

--- i3blocks/mpd.sh
+++ i3blocks/mpd.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -e ~/.config/mpd/host ]; then
-  MPD_HOST=$(cat ~/.config/mpd/host)
-else
-  MPD_HOST="localhost"
-fi
-
-if mpc -h "$MPD_HOST" status | grep playing > /dev/null; then
+if mpc status | grep playing > /dev/null; then
   printf " "
-  mpc -h "$MPD_HOST" current
+  mpc current
 fi
