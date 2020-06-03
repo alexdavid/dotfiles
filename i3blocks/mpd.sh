d850859ff3e48d59a265b2c58c9524582f2d65d7
Alex David <git@alexd.me>
2020-06-03 12:36:00 -0700

    Fix mpd i3block if mpd is not connectable

--- i3blocks/mpd.sh
+++ i3blocks/mpd.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-which mpc &> /dev/null || exit 0
-
 while true; do
+  if ! mpc idle > /dev/null; then
+    sleep 120
+  fi
   if mpc status | grep playing > /dev/null; then
     echo " $(mpc current)"
   else
     echo
   fi
-  mpc idle > /dev/null
 done
