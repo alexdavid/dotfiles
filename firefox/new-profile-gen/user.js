80620854fcf61a0e9666625f7e9255af8012be05
Alex David <git@alexd.me>
2021-01-08 10:19:59 -0800

    Update firefox user.js

--- firefox/new-profile-gen/user.js
+++ firefox/new-profile-gen/user.js
@@ -1,9 +1,6 @@
 // In wayland firefox smoothscrolling is not really needed since scrolling is such high resolution
 user_pref("general.smoothScroll", false);
 
-// Set accel key to alt
-user_pref("ui.key.accelKey", 18);
-
 // Enable encrypted SNI
 user_pref("network.security.esni.enabled", true);
 
@@ -20,11 +17,13 @@ user_pref("browser.aboutConfig.showWarning", false);
 // Set toolbar
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"downloads-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\"],\"currentVersion\":16,\"newElementCount\":3}");
 
-// Enable userchrome & set dark theme
+// Enable userchrome
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
-user_pref("ui.systemUsesDarkTheme", 1);
-user_pref("devtools.theme", "dark");
+
+// Set dark theme
+// user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
+// user_pref("ui.systemUsesDarkTheme", 1);
+// user_pref("devtools.theme", "dark");
 
 // Disable new-tab page garbage
 user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
@@ -41,3 +40,6 @@ user_pref("datareporting.healthreport.uploadEnabled", false);
 
 // Fixes rendering issues with transparent window elements
 user_pref("layers.acceleration.force-enabled", true);
+
+// Disable privacy tab from opening on new profiles
+user_pref("datareporting.policy.firstRunURL", "");
