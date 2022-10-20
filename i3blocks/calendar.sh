6529bac3f0f2ab5a1bde664cc44dd65506592673
Alex David <git@alexd.me>
2022-10-19 23:10:20 -0700

    Update khal bar format

--- i3blocks/calendar.sh
+++ i3blocks/calendar.sh
@@ -2,7 +2,7 @@
 
 which khal &> /dev/null || exit 0
 
-event="$(khal list --day-format '' --format '{title} @ {start-time}' now eod)"
+event="$(khal list --day-format '' --format '{start-time} {title}' now eod)"
 if [ "$event" != "No events" ]; then
   echo " $event" | head -n1
 fi
